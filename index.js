f8d50d72b0c30c9b2837bb061dd0de7698878d67
Isna Yousuf <isnayousuf77@gmail.com>
2022-01-10 20:51:26 +0500

    add pronunciation

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ let first =document.querySelector('.newWord');
 let second =document.querySelector('.newWord2');
 let third =document.querySelector('.newWord3');
 let fourth =document.querySelector('.newWord4');
-
-
+let audioElement = document.querySelector('.pronounce');
+let emptyInput = document.querySelector('.noData');
+let cross = document.getElementById('cross');
 function logData(){
     let a = input.value;
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${a}`
@@ -21,17 +22,36 @@ let a= result[0].meanings[0].definitions[0];
 let meaning = result[0].meanings[0].definitions[0].definition;
 let example = result[0].meanings[0].definitions[0].example;
 
-console.log(word);
-console.log(history);
-console.log(meaning);
-console.log(example);
 
-// const audio = result[0].phonetics[0].audio;
+const audio = result[0].phonetics[0].audio;
+audioElement.src= audio;
+
 first.innerText= `${word.toUpperCase()}`;
 second.innerText =`Origin: ${history}`;
 third.innerText=`Meaning: ${meaning}`;
 fourth.innerText =`Example: ${example}`;
 wordContainer.classList.remove('hide')
+
+// function noWord(){
+//     if(input.value != `result[0].word`){
+//         alert('this is not a word')
+//     }
+
+
 }
+
 }
-btn.addEventListener('click', logData)
+
+function emptyWord(){
+    if(input.value == ''){
+    wordContainer.classList.add('hide');
+    emptyInput.classList.remove('hide');
+
+    }
+}
+function hideWarning() {
+    emptyInput.classList.add('hide');
+}
+cross.addEventListener('click', hideWarning);
+btn.addEventListener('click', emptyWord);   //handling empty words
+btn.addEventListener('click', logData);   //handling words     
